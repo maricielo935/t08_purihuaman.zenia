3e59bb1cee397aa47778976e194d96416a6d6693
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-12-12 21:49:52 -0500

    se agrego 15 ejercicios de tipo iteracion parte 2

---
+++ zenia.tunioque/iteracion2.py
@@ -0,0 +1,74 @@
+#Ejercicio1:
+msg="HOLA ME LLAMO ELMER ZENIA TONIOQUE, MUCHO GUSTO , ¿COMO ESTAS?"
+for letra in msg:
+    print(letra)
+
+#Ejercicio2:
+msg="BUENAS TARDES, A LA HORA QUE LLEGUES, ME LLAMAS, NECESITAMOS HABLAR"
+for letra in msg:
+    print(letra)
+
+#Ejercicio3:
+msg="CON MUCHO ESFUERZO Y DEDICACION SE QUE PODRE APROBAR EL CURSO DE PROGRAMACION"
+for letra in msg:
+    print(letra)
+
+#Ejercicio4:
+msg="LA OBRA PACO YUNQUE FUE ESCRITA POR EL AUTOR PERUANO CESAR VALLEJO"
+for letra in msg:
+    print(letra)
+
+#Ejercicio5:
+msg="INGRESE A LA UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO, A LA CARRERA PROFESIONAL DE INGENIERIA ELECTRONICA"
+for letra in msg:
+    print(letra)
+
+#Ejercicio6:
+msg="MI ABUELO RAUL SE FUE AL CAMPO A COSECHAR ARROZ , PAPA Y CAMOTE"
+for letra in msg:
+    print(letra)
+
+#Ejercicio7:
+msg="SE QUE LAS COSAS NO ESTAN DEL TODO BIEN, POR ESO QUISIERA QUE HABLEMOS, POR FAVOR"
+for letra in msg:
+    print(letra)
+
+#Ejercicio8:
+msg="SI CUANDO TU ESTABAS, ME SENTIA VACIO, AHORA QUE TU NO ESTAS ME SIENTO LLENO"
+for letra in msg:
+    print(letra)
+
+#Ejercicio9:
+msg="LUCIA Y ROSAELVIRA FUERON A UNA BODEGA A COMPRAR GALLETAS Y GASEOSAS, PARA EL CUMPLEANIOS DE FATIMA"
+for letra in msg:
+    print(letra)
+
+#Ejercicio10:
+msg="PAMELA ES EL GRAN AMOR DE MI VIDA, AUNQUE QUIZA YO NO SEA EL SUYO"
+for letra in msg:
+    print(letra)
+
+#Ejercicio11:
+msg="EN MIS VACACIONES DE VERANO, MI FAMILIA Y YO, VIAJAREMOS A LAS PLAYAS DE TUMBES Y PIURA"
+for letra in msg:
+    print(letra)
+
+#Ejercicio12:
+msg="MACHU PICHU ES UNA DE LAS SIETE MARAVILLAS DEL MUNDO, Y SE ENCUENTRA EN EL PERU"
+for letra in msg:
+    print(letra)
+
+#Ejercicio13:
+msg="GALLESE SI LE FUE INFIEL A SU ESPOSA, AUNQUE EL LO NIEGUE UNA Y MIL VECES FRENTE A CAMARAS"
+for letra in msg:
+    print(letra)
+
+#Ejercicio14:
+msg="TODOS LOS DIAS ME EMBARCO EN UN PARQUE QUE ESTA CERCA DE MI CASA, UN PARADERO, QUE ME LLEVARA A LAMBAYEQUE"
+for letra in msg:
+    print(letra)
+
+#Ejercicio15:
+msg="A VECES LAS COSAS NO PASAN COMO UNO LAS ESPERA, PERO SI PASAN POR ALGO ES POR UNA BUENA RAZON"
+for letra in msg:
+    print(letra)
