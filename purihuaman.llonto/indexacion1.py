dc2586e234d3cb9ccb82765840edbb2cc5069706
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-12-12 21:37:05 -0500

    se agrego 15 ejercicios de tipo funciones de indexacion

---
+++ purihuaman.llonto/indexacion1.py
@@ -0,0 +1,103 @@
+#EJERCICIO 1:
+#              1         2         3         4         5         6
+#    012345678901234567890123456789012345678901234567890123456789012345
+msg="SI ALGUIEN TE HACE VOLAR ASEGURATE DE CAER DE PIE CUANDO TE SUELTE"
+print(msg[51])                               #DEVUELVE U
+
+
+#EJERCICIO 2:
+#              1         2
+#    012345678901234567890123
+msg="LOOK WHAT YOU MADE ME DO"
+print(msg[10])                               #DEVUELVE Y
+
+
+#EJERCICIO 3:
+#              1         2         3         4
+#    0123456789012345678901234567890123456789012
+msg="LA BESE TANTAS VECES BAJO EL CIELO INFINITO"
+print(msg[8])                                #DEVUELVE T
+
+
+#EJERCICIO 4:
+#              1         2         3         4         5         6         7
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456
+msg="NO SOMOS FRIOS POR FALTA DE EMOCIONES, SI NO POR LA ABUNDANCIA DE DECEPCIONES"
+print(msg[21])                               #DEVUELVE L
+
+
+#EJERCICIO 5:
+#              1         2         3         4
+#    012345678901234567890123456789012345678901234
+msg="YO LA QUISE, Y A VECES ELLA TAMBIEN ME QUERIA"
+print(msg[39])                               #DEVUELVE Q
+
+
+#EJERCICIO 6:
+#              1         2         3         4         5         6
+#    0123456789012345678901234567890123456789012345678901234567890123456
+msg="SOLO UNA COSA CONVIERTE EN IMPOSIBLE UN SUENIO, EL MIEDO A FRACASAR"
+print(msg[51])                               #DEVUELVE M
+
+
+#EJERCICIO 7:
+#              1         2         3         4
+#    01234567890123456789012345678901234567890123
+msg="ROZARON SUS MIRADAS, PERO FINGIERON NO VERSE"
+print(msg[37])                               #DEVUELVE O
+
+
+#EJERCICIO 8:
+#              1         2         3
+#    012345678901234567890123456789012345
+msg="TIENES QUE DEJAR IR LO QUE YA SE FUE"
+print(msg[13])                               #DEVUELVE J
+
+
+#EJERCICIO 9:
+#              1         2         3         4         5
+#    01234567890123456789012345678901234567890123456789012345678
+msg="NI NACI CUANDO TE CONOCI, NI ME VOY A MORIR CUANDO TE VAYAS"
+print(msg[34])                               #DEVUELVE Y
+
+
+#EJERCICIO 10:
+#              1         2         3         4         5
+#    012345678901234567890123456789012345678901234567890
+msg="SE TU MISMO, Y LA GENTE CORRECTA SE QUEDARA CONTIGO"
+print(msg[7])                                #DEVUELVE I
+
+
+#EJERCICIO 11:
+#              1         2         3         4         5         6
+#    0123456789012345678901234567890123456789012345678901234567890123456789
+msg="UNA VEZ QUE ACEPTAS TUS DEFECTOS, NADIE PODRA UTILIZARLOS EN TU CONTRA"
+print(msg[15])                               #DEVUELVE P
+
+
+#EJERCICIO 12:
+#              1         2         3         4         5         6         7         8         9
+#    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+msg="LA MUERTE ES TAN TERRIBLEMENTE DEFINITIVA, MIENTRAS QUE LA VIDA ESTA LLENA DE POSIBILIDADES"
+print(msg[90])                               #DEVUELVE S
+
+
+#EJERCICIO 13:
+#              1         2
+#    01234567890123456789012345
+msg="EL AMOR ES TODO MENOS DUDA"
+print(msg[24])                               #DEVUELVE D
+
+
+#EJERCICIO 14:
+#              1         2         3         4         5         6
+#    0123456789012345678901234567890123456789012345678901234567890123456789
+msg="ASI COMO HAY COSAS QUE PASAN POR ALGO, HAY COSAS QUE POR ALGO NO PASAN"
+print(msg[39])                               #DEVUELVE H
+
+
+#EJERCICIO 15:
+#              1         2         3         4         5
+#    01234567890123456789012345678901234567890123456789012
+msg="CUALQUIER COSA ES POSIBLE SI TIENES SUFICIENTE CORAJE"
+print(msg[38])                               #DEVUELVE F
