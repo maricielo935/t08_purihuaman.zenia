b375c0ace0d28fb01d07a824baf15c43f9bde75e
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-12-12 21:46:50 -0500

    se agrego 15 ejercicios de tipo concatenacion parte 2

---
+++ zenia.tunioque/concatenacion2.py
@@ -0,0 +1,104 @@
+#Ejercicio1:
+#              1         2         3         4
+#    01234567890123456789012345678901234567890123456
+msg="LOS AMIGOS NO SE LLAMAN A LAS DOS DE LA MANIANA"
+print(msg[40:45])
+#Devuelve MANIA
+
+#Ejercicio2:
+#              1         2         3         4
+#    0123456789012345678901234567890123456789012
+msg="MI NOMBRE COMPLETO ES: ELMER ZENIA TUNIOQUE"
+print(msg[10:12]+msg[13]+msg[32]+msg[33])
+#Devuelve COPIA
+
+#Ejercicio3:
+#              1         2         3         4         5
+#    012345678901234567890123456789012345678901234567890123456789
+msg="MI PELICULA FAVORITA ES HARRY POTTER Y LOS JUEGOS DEL HAMBRE"
+print(msg[30:32]+msg[16]+msg[18]+msg[4]+msg[16:14:-1])
+#Devuelve PORTERO
+
+#Ejercicio4:
+#              1         2         3         4         5
+#    0123456789012345678901234567890123456789012345678901234
+msg="ME GUSTA ESCUCHAR MUSICA, MAS AUN SI ES SALSA O BACHATA"
+print(msg[0]+msg[4]+msg[1]+msg[16]+msg[53]+msg[37])
+#Devuelve MUERTE
+
+#Ejercicio5:
+#              1         2         3
+#    0123456789012345678901234567890123456789
+msg="TENGO 19 ANIOS Y AUN VIVO CON MIS PADRES"
+print(msg[21:23]+msg[36]+msg[9])
+#Devuelve VIDA
+
+#Ejercicio6:
+#              1         2         3         4         5
+#    0123456789012345678901234567890123456789012345678901
+msg="ANTONY SE FUE A UNA FIESTA A LA CUAL NO ME INVITARON"
+print(msg[0]+msg[45:47]+msg[38:36:-1])
+#Devuelve AVION
+
+#Ejercicio7:
+#              1         2         3         4         5         6
+#    012345678901234567890123456789012345678901234567890123456789012345678
+msg="A ALTAS HORAS DE LA MADRUGADA UN CHICO LE DIO SERENATA A SU ENAMORADA"
+print(msg[8]+msg[64]+msg[17:19])
+#Devuelve HOLA
+
+#Ejercicio8:
+#              1         2         3         4         5         6
+#    01234567890123456789012345678901234567890123456789012345678901234567
+msg="SE QUE COMETO MUCHOS ERRORES, PERO QUISIERA REMEDIARLOS Y NO SE COMO"
+print(msg[14]+msg[42]+msg[14]+msg[49])
+#Devuelve MAMI
+
+#Ejercicio9:
+#              1         2         3
+#    012345678901234567890123456789012345678
+msg="ARIANA ES UNA GRAN AMIGA DE LA INFANCIA"
+print(msg[28:39])
+#Devuelve LA INFANCIA
+
+#Ejercicio10:
+#              1         2         3
+#    0123456789012345678901234567890123456
+msg="DI QUE TE VAS, Y QUE YA NO REGRESARAS"
+print(msg[10]+msg[5]+msg[7:9])
+#Devuelve VETE
+
+#Ejercicio11:
+#              1         2         3         4         5
+#    012345678901234567890123456789012345678901234567890123456
+msg="Y NO ME VOY A PERDONAR SI CONOCERTE FUE UNA CAUSA PERDIDA"
+print(msg[14:20])
+#Devuelve PERDON
+
+#Ejercicio12:
+#              1         2         3         4         5         6         7
+#    012345678901234567890123456789012345678901234567890123456789012345678901
+msg="SE QUE GANAREMOS LAS OLIMPIADAS ESTE ANIO, LO HAREMOS JUNTOS COMO EQUIPO"
+print(msg[25]+msg[11]+msg[14]+msg[7]+msg[11]+msg[8]+msg[13]+msg[8]+msg[61]+msg[69]+msg[71]+msg[38])
+#Devuelve PROGRAMACION
+
+#Ejercicio13:
+#              1         2         3
+#    012345678901234567890123456789012345678
+msg="MI PLATO PREFERIDO ES EL ARROZ CON PATO"
+print(msg[12:16]+msg[5]+msg[16:18])
+#Devuelve FERIADO
+
+#Ejercicio14:
+#              1         2         3         4         5         6         7
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789
+msg="OJALA PUDIERA SER FEO UN DIA, PORQUE SERLO TODOS LOS DIAS YA NO ME ESTA GUSTANDO"
+print(msg[33:36]+ " " +msg[43]+msg[4:2:-1])
+#Devuelve QUE TAL
+
+#Ejercicio15:
+#              1         2         3         4         5         6
+#    0123456789012345678901234567890123456789012345678901234567890
+msg="EL PROFESOR DE TECNICAS DE ESTUDIO DEJO UN TRABAJO: UN ENSAYO"
+print(msg[0:2]+ " " +msg[15:17]+msg[28]+msg[33]+msg[10:8:-1])
+#Devuelve EL TESORO
