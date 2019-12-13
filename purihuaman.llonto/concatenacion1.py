b71bd6feee7f2fadb1716d48c11edc5fb822bb54
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-12-12 21:34:39 -0500

    se agrego 15 ejercicios de tipo concatenacion

---
+++ purihuaman.llonto/concatenacion1.py
@@ -0,0 +1,103 @@
+#EJERCICIO 1:
+#              1         2         3         4         5         6         7         8         9
+#    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
+msg="PUEDO ESCRIBIR LOS VERSOS MAS TRISTE ESTA NOCHE, PENSAR QUE NO LA TENGO, SENTIR QUE LA HE PERDIDO"
+print(msg[0]+msg[4]+msg[15]+msg[15]+msg[16]+ " " +msg[27]+ " " + msg[15]+msg[27]+ " " +msg[49]+msg[84:86]+msg[42]+msg[44:46]+msg[27])                       #Devuelve POLLO A LA PLANCHA
+
+
+#EJERCICIO 2:
+#              1         2         3         4         5         6         7         8         9         1
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+msg="YO SOY UN HOMBRE SINCERO DE DONDE CRECE LA PALMA Y ANTES DE MORIRME QUIERO HECHAR MIS VERSOS DEL ALMA"
+print(msg[62:64]+msg[20]+msg[79:81]+msg[28:30]+ " " +msg[43:48])                                                                                            #Devuelve RICARDO PALMA
+
+
+#EJERCICIO 3:
+#              1         2         3         4         5         6         7         8         9         1
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
+msg="JUVENTUD DIVINO TESORO, YA TE VAS PARA NO VOLVER, CUANDO QUIERO LLORAR, NO LLORO Y A VECES LLORO SIN QUERER"
+print(msg[27:49]+msg[81:107])                                                                                                                               #Devuelve TE VAS PARA NO VOLVER, Y A VECES LLORO SIN QUERER
+
+
+#EJERCICIO 4:
+#              1         2         3         4         5         6         7         8         9         10        11        12        13
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
+msg="LA HUMANIDAD TIENE DEBILIDADES, PERO DOS DE LAS MAS IMPORTANTES SON; LA INCAPACIDAD DE LLEGAR A TIEMPO Y LA INEPTITUD DE CUMPLIR PROMESAS"
+print(msg[1]+ " " + msg[48]+msg[6]+ msg[23]+ " " +msg[96:101]+msg[38]+ " " +msg[21]+msg[4]+msg[15]+msg[7]+msg[6]+ " " +msg[74:76]+msg[92:90:-1])            #Devuelve A MAL TIEMPO BUENA CARA
+
+
+#EJERCICIO 5:
+#              1         2         3         4         5         6         7         8         9         10
+#    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
+msg="TE DARAS CUENTA QUE LO QUE HOY PARECE UN SACRIFICIO, MANIANA TERMINARA SIENDO EL MAYOR LOGRO DE TU VIDA"
+print(msg[7]+msg[1]+msg[5]+ " " +msg[21]+ " " +msg[31:37]+msg[44])                                                                                          #Devuelve SER O PARECER
+
+
+#EJERCICIO 6:
+#              1         2         3         4         5         6
+#    0123456789012345678901234567890123456789012345678901234567890123
+msg="PORQUE ME AFERRO A QUE NO FUE UN FINAL SINO UN ALTO EN EL CUENTO"
+print(msg[10:15]+msg[10]+msg[49]+msg[5]+ " " +msg[10]+ " " +msg[10]+msg[3:6]+msg[37]+msg[37]+msg[1]+ " " +msg[19:22]+ " " +msg[17]+msg[7]+msg[17]+msg[39])  #Devuelve AFERRATE A AQUELLO QUE AMAS
+
+
+#EJERCICIO 7:
+#              1         2         3         4
+#    012345678901234567890123456789012345678901234
+msg="YO LA QUISE, Y A VECES ELLA TAMBIEN ME QUERIA"
+print(msg[23:25]+ " " +msg[31:35]+ " " +msg[13]+ " " +msg[23:25]+ " " +msg[30]+msg[26:24:-1])                                                               #Devuelve EL BIEN Y EL MAL
+
+
+#EJERCICIO 8:
+#              1         2         3         4         5         6         7         8
+#    01234567890123456789012345678901234567890123456789012345678901234567890123456789012
+msg="LA GRANDEZA INSPIRA ENVIDIA, LA ENVIDIA ENGRANDA RENCOR Y EL RENCOR GENERA MENTIRAS"
+print(msg[58:60]+ " " +msg[61:67]+ " " +msg[68:74]+ " " + msg[20:27])                                                                                       #Devuelve EL RENCOR GENERA ENVIDIA
+
+
+#EJERCICIO 9:
+#              1         2         3         4         5         6         7         8         9         10        11        12        13
+#    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345
+msg="OH, CUANTO TIEMPO SILENCIOSA EL ALMA, MIRA ENEREDEDOR SU SOLEDAD QUE AUMENTA, COMO UN PENDULO INMOVIL, YA NO CUENTA LAS HORAS QUE SE VAN"
+print(msg[4]+msg[12]+msg[21:23]+ " " +msg[6:8]+msg[12]+msg[25:27]+ " " +msg[48:50]+ " " +msg[57:64])                                                        #Devuelve CIEN ANIOS DE SOLEDAD
+
+
+#EJERCICIO 10:
+#              1         2         3         4         5         6         7         8
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
+msg="SOY EL CANTOR DE AMERICA, AUTOCTONO Y SALVAJE, MI LIRA TIENE UN ALMA, MI CANTO UN IDEAL"
+print(msg[7]+msg[4]+msg[38]+msg[8]+msg[20]+ " " +msg[41:43]+msg[40]+msg[40]+msg[4]+msg[43]+msg[34])                                                         #Devuelve CESAR VALLEJO
+
+
+#EJERCICIO 11:
+#              1         2         3         4         5         6         7
+#    012345678901234567890123456789012345678901234567890123456789012345678901
+msg="QUE ALGUIEN NO HAYA RECONOCIDO TU VALOR NO QUIERE DECIR QUE NO LO TENGAS"
+print(msg[20:27]+msg[2]+ " " +msg[31:33]+ " " +msg[9]+msg[20]+msg[20]+msg[13]+msg[20])                                                                      #Devuelve RECONOCE TU ERROR
+
+
+#EJERCICIO 12:
+#              1         2         3         4         5         6         7         8         9         10
+#    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345
+msg="UN AMIGO ES ALGUIEN QUE PUEDE VER LA VERDAD EN TI, INCLUSO HASTA CUANDO ESTAS ENGANIANDO A TODOS LOS DEMAS"
+print(msg[93]+msg[5]+msg[7]+msg[56]+ " " +msg[9:11]+ " " +msg[14]+msg[32]+msg[84:87]+msg[44])                                                               #Devuelve DIOS ES GRANDE
+
+
+#EJERCICIO 13:
+#              1         2         3         4         5         6         7         8
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567890123
+msg="EL ARTE ES LA EXPRESION DE LOS MAS PROFUNDOS PENSAMIENTOS POR EL CAMINO MAS SENCILLO"
+print(msg[45:49]+msg[3]+msg[4]+ " " +msg[3]+msg[22]+msg[5:7]+msg[9]+ " " +msg[24:26]+ " " +msg[3]+msg[65]+msg[5]+msg[39]+msg[3:5])                          #Devuelve PENSAR ANTES DE ACTUAR
+
+
+#EJERCICIO 14:
+#              1         2         3         4         5         6         7         8
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567890123
+msg="TODOS NUESTROS SUENIOS PUEDEN HACERSE REALIDAD, SI TENEMOS EL CORAJE DE PERSEGUIRLOS"
+print(msg[0:5]+ " " + msg[4]+msg[1]+msg[55]+msg[1]+msg[4]+ " " +msg[30]+msg[7]+msg[55]+msg[31]+msg[28]+msg[56:58])                                          #Devuelve TODOS SOMOS HUMANOS
+
+
+#EJERCICIO 15:
+#              1         2         3         4         5         6         7         8
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567890
+msg="PARA QUE LOS CAMBIOS TENGAN UN VALOR VERDADERO DEBEN SER CONSISTENTES Y DURADEROS"
+print(msg[7]+msg[9]+ " " +msg[1]+msg[15]+msg[10]+msg[35]+ " " +msg[37:46]+ " " +msg[72:76]+ " " +msg[21]+msg[17]+msg[7]+msg[15]+msg[0]+msg[10])             #Devuelve EL AMOR VERDARO DURA TIEMPO
